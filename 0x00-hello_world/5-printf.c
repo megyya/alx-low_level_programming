d17fd4450cdaf0d0fd29f4ae3246e933af79f172
megyya <myabdelmawgoud@gmail.com>
2023-03-16 14:25:00 -0700

    printf c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of the program that uses printf and not puts
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
