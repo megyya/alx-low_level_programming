284e5d859c30de486eede7dff8b1b317f3790bc1
megyya <myabdelmawgoud@gmail.com>
2023-03-16 13:59:14 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of the program that uses puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
